942696dccba3680bffa2146f4e5e382976990d4d
Jahnavi durga Ganapathi <164031327+ganapathijahnavi@users.noreply.github.com>
2026-08-16 21:02:20 +0530

    Time: 47 ms (72.52%), Space: 30.7 MB (31.3%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        cnt = [0,0,0]
+        for s in stones:
+            cnt[s%3] += 1
+        if cnt[0] % 2 == 0:
+            return min(cnt[1],cnt[2]) >= 1
+        return abs(cnt[1] - cnt[2]) >= 3
+        
+
+        
